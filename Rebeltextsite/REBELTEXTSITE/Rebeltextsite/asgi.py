cc922a1a7bc970a109f030ee626299d5ce406521
Rebelhere <muhammadrahimusman@gmail.com>
2025-02-09 11:35:20 +0500

    some changes because of errors

--- Rebeltextsite/REBELTEXTSITE/Rebeltextsite/asgi.py
+++ Rebeltextsite/REBELTEXTSITE/Rebeltextsite/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'textutiles.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Rebeltextsite.settings')
 
 application = get_asgi_application()
